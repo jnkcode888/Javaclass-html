2cbf381c271d600ce1498b46c212c4f7350911ae
jnkcode888 <kinyuajohnnjoroge@gmail.com>
2024-10-16 15:37:37 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const javaclass=document.getElementById('colorButton')
+//get the button element
+const javaclasS=Document.getElementById('colorButton')
+
+
+javaclass.addEventListener('click',()=>{
+    //change the color
+    javaclass.style.backgroundColor=javaclass.style.backgroundColor==='blue' ? '#4CAF50':'blue';
+})
